69cf876922547f0d0604847a8ca0b39a513238e1
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-04-16 14:17:05 +0000

    Ex02: Empty layout

---
+++ app/src/main/java/com/anouarderdouri/m213_ch07_tp01/Ex02Activity.java
@@ -0,0 +1,14 @@
+package com.anouarderdouri.m213_ch07_tp01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ex02Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex02);
+    }
+}
